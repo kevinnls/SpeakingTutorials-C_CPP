befa0abe5f3b9835359cd8349a23e81bc6d7e2ce
kevinnls <developer@kevinnlsamuel.com>
2023-04-30 15:10:44 +0530

    First C Program / 0

---
+++ talk.c
@@ -0,0 +1,12 @@
+// My First C Program in this course
+// - single line comment
+
+#include <stdio.h>
+
+// # main
+// - function where program starts
+// - only one main may exist
+int main() {
+	printf("Talk to a teacher\n");
+	return 0;
+}
