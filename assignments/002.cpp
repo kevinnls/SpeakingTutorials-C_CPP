efa21a602875e43a10497b71db75f8aae90e001b
kevinnls <developer@kevinnlsamuel.com>
2023-04-30 15:38:41 +0530

    Assignment 002

---
+++ assignments/002.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Kevin\n";
+	/*do not put personal information in
+	 * publicly accessible code files
+	 */
+	std::cout << "[redacted for privacy]\n";
+	return 0;
+}
---
+++ talk.cpp
@@ -0,0 +1,10 @@
+// My first C++ program in this course
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout <<"Talk to a teacher\n";
+	return 0;
+}
